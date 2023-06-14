cf937fb10cfcde2195248abf07dae51fb9b170d0
Paweł Chuchmała <pawel.chuchmala@digitalvirgo.com>
2023-06-14 17:15:20 +0200

    endlessReality model

--- setup.sh
+++ setup.sh
@@ -21,6 +21,9 @@ cd ..
 mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
 rm -rf stable-diffusion-2-1-base/
 
+curl https://chuchmala.pl/download/endlessReality --output endlessReality
+mv endlessReality stable-diffusion-webui/models/Stable-diffusion/
+
 # download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
 wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
 cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
@@ -30,4 +33,3 @@ sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
 
 # start the server as user 'ubuntu'
 sudo -u ubuntu nohup bash stable-diffusion-webui/webui.sh --listen > log.txt
-
